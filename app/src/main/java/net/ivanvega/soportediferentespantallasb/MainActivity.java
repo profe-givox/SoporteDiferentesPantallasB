65d48d73c047589b7b189dcda0ffe8164fa1c963
profe-givox <gi.vega@itsur.edu.mx>
2021-09-17 14:38:21 -0500

    Initial commit

---
+++ app/src/main/java/net/ivanvega/soportediferentespantallasb/MainActivity.java
@@ -0,0 +1,17 @@
+package net.ivanvega.soportediferentespantallasb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
