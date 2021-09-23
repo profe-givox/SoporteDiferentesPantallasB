8c984e0dffb4a49e1c815328c34fbcf44f42e9be
profe-givox <gi.vega@itsur.edu.mx>
2021-09-23 14:56:54 -0500

    Se agrego Recycler View

---
+++ app/src/main/java/net/ivanvega/soportediferentespantallasb/Libro.java
@@ -0,0 +1,13 @@
+package net.ivanvega.soportediferentespantallasb;
+
+public class Libro {
+    private String titulo;
+    private String autor;
+    private int recursoImagen;
+    private String url;
+    private String genero;
+
+    private Boolean novedad;
+    private  Boolean leido;
+
+}
--- app/src/main/java/net/ivanvega/soportediferentespantallasb/MiAdaptadorPersonaliza.java
+++ app/src/main/java/net/ivanvega/soportediferentespantallasb/MiAdaptadorPersonaliza.java
@@ -1,22 +1,27 @@
 package net.ivanvega.soportediferentespantallasb;
 
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView.Adapter;
+
+import java.util.Vector;
 
 public class MiAdaptadorPersonaliza extends
-        RecyclerView.Adapter
-{
+        Adapter<MiAdaptadorPersonaliza.ViewHolder>{
+    private Vector<Libro> libros;
+
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         return null;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
     }
 
@@ -24,4 +29,14 @@ public class MiAdaptadorPersonaliza extends
     public int getItemCount() {
         return 0;
     }
+
+    public  static class ViewHolder extends
+                RecyclerView.ViewHolder {
+
+            public ViewHolder(@NonNull View itemView) {
+                super(itemView);
+
+            }
+        }
+
 }
